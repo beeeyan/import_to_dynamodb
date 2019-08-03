ecf44f60f85f91a2e59c3c78e5df3985f53f1009
sakasanokuma <moonsunphoenix@gmail.com>
2019-08-03 22:54:08 +0900

    新しいテーブル名を指定できるように変更

--- create_insert_command.py
+++ create_insert_command.py
@@ -4,13 +4,19 @@ import sys
 import datetime
 
 if len(sys.argv) < 2:
-    table_name = 'importtest'
+    old_table_name = 'importtest'
+    new_table_name = old_table_name
+elif len(sys.argv) == 2:
+    old_table_name = sys.argv[1]
+    new_table_name = old_table_name
 else:
-    table_name = sys.argv[1]
+    old_table_name = sys.argv[1]
+    new_table_name = sys.argv[2]
+
 
 now_date = datetime.datetime.today().strftime("%Y%m%d%H%M%S")
-template = 'aws dynamodb put-item --table-name ' + table_name + ' --item'
-csv_input = pd.read_csv(table_name + '.csv', dtype="str")
+template = 'aws dynamodb put-item --table-name ' + new_table_name + ' --item'
+csv_input = pd.read_csv(old_table_name + '.csv', dtype="str")
 
 for index, row in csv_input.iterrows():
     item_content = ''
@@ -19,5 +25,5 @@ for index, row in csv_input.iterrows():
             item_content = item_content + '"' + row.index[i].split(' ')[0] + '" : { "' + row.index[i].split(' ')[1].strip('\(''\)') + '" : "' + str(row[i]) + '" }, '
     item_content = ' \'{ ' + item_content.rstrip(', ') + ' }\''
     print(str(index+1) + ' line')
-    with open(table_name + '_import_' + now_date +'.sh', 'a') as f:
+    with open(new_table_name + '_import_' + now_date +'.sh', 'a') as f:
         f.write(template + item_content + '\n')
